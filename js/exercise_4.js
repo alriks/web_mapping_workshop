d6f18e1b2013506521e5e3cb7ab45cab4c94e207
alriks <alriks@users.noreply.github.com>
2016-05-02 15:11:12 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -25,7 +25,7 @@ var featureLayer= L.mapbox.featureLayer();
 
 featureLayer.on('ready', function(){
 	this.eachLayer(function(){
-    	Layer.setIcon(L.mapbox.marker.icon({
+    	featureLayer.setIcon(L.mapbox.marker.icon({
         	"marker-color": "#8834bb",
           	"marker-size": "small",
           	"marker-symbol": "restaurant"
