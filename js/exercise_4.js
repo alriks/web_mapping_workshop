48858eb690838c212f5a64b6dde65e2954fda0ae
alriks <alriks@users.noreply.github.com>
2016-05-02 17:26:00 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -60,10 +60,7 @@ var clickHandler= function (e){
     })
 }
 
-  $('#info').append(info);
-
 var myGeoJSON = myLocation.getGeoJSON();
-
 getDirections(myGeoJSON.geometry.coordinates, feature.geometry.coordinates);
       
 featureLayer.on('ready',function(){
