1f7caf0fa3ebc3d36492221daea2e2d671bc5dc0
alriks <alriks@users.noreply.github.com>
2016-05-02 15:24:23 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -36,5 +36,6 @@ featureLayer.on('ready', function(){
 
 featureLayer.on('ready',function(){
   	this.eachLayer(function(layer){
-    	layer.bindPopup('Welcome to 'layer.feature.properties.name)
-        }))
+    	layer.bindPopup('Welcome to ' layer.feature.properties.name);
+        })
+	})
